5c49e2005155af3034f0fe62b9675b9af813ae23
JWoltjen <Jeff.Woltjen@gmail.com>
2021-10-19 09:44:08 -0500

    create global style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import {ThemeProvider} from 'styled-components'
 import Header from './components/Header'
 import {Container} from './components/styles/container.styled'
+import GlobalStyles from './components/styles/Global'
+
 
 const theme = {
   colors: {
     header: `#ebfbff`, 
     body: `#fff`,
-    footer: `#003333`
-  }
+    footer: `#003333`,
+  },
 }
 
 function App() {
   return (
     <ThemeProvider theme = {theme}>
     <>
+    <GlobalStyles />
     <Header />
     <Container>
       <h1>Hello World</h1>
