540c3421b69d2d462d44d8ecf2415f057b7d445b
JWoltjen <Jeff.Woltjen@gmail.com>
2021-10-19 09:17:11 -0500

    create color theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import {ThemeProvider} from 'styled-components'
 import Header from './components/Header'
 import {Container} from './components/styles/container.styled'
 
+const theme = {
+  colors: {
+    header: `#ebfbff`, 
+    body: `#fff`,
+    footer: `#003333`
+  }
+}
+
 function App() {
   return (
+    <ThemeProvider theme = {theme}>
     <>
     <Header />
     <Container>
       <h1>Hello World</h1>
     </Container>
     </>
+    </ThemeProvider>
   );
 }
 
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -3,4 +3,9 @@ import styled from 'styled-components'
 export const StyledHeader = styled.header`
 background-color: #ebfbff; 
 padding: 40px 0; 
+
+
+h1 {
+    color: red; 
+}
 `
